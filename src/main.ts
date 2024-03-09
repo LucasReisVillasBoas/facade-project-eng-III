4ecce5d5f2121c7a7beb73fddbbd5de0d64917dc
Lucas Reis Villas Boas <lucas.reisvb@gmail.com>
2024-03-09 01:21:37 -0300

    politica cors atualizada

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const corsOptions: CorsOptions = {
+    origin: 'http://localhost:3001', // Permitir apenas solicitações da origem http://localhost:3001
+    credentials: true, // Permitir credenciais (cookies, cabeçalhos de autenticação)
+  };
+
+  app.enableCors(corsOptions);
+
   await app.listen(3000);
 }
 bootstrap();
